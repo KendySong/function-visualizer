59b1baeede8a6bee188482104818caf535a9450e
Kendy Song <kendy.song@solmani.ch>
2023-05-17 16:47:02 +0200

    Update orbital camera

--- src/Graphics/OrbitCamera.cpp
+++ src/Graphics/OrbitCamera.cpp
@@ -17,18 +17,18 @@ void OrbitCamera::processMovements(float deltaTime, glm::vec2 mouse)
 	if (glfwGetMouseButton(p_window, GLFW_MOUSE_BUTTON_RIGHT))
 	{
 		glfwSetInputMode(p_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-		glm::vec2 offset = mouse - m_last;
-		offset *= SENSITIVITY;
-		m_rotation += offset;
+		m_rotation += (mouse - m_last) * SENSITIVITY;
 
-		m_position.x = sin(m_rotation.x);
-		m_position.z = cos(m_rotation.x);
-
-		m_last = mouse;
-		this->view = glm::lookAt(m_position * m_distance, m_center, m_up);		
+		m_position.x = sin(m_rotation.x) * cos(m_rotation.y);
+		m_position.y = sin(m_rotation.y);
+		m_position.z = cos(m_rotation.x) * cos(m_rotation.y);
+			
+		this->view = glm::lookAt(m_position * m_distance + glm::vec3(0, 1, 0), m_center, m_up);
 	}
 	else
 	{
 		glfwSetInputMode(p_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 	}
+
+	m_last = mouse;
 }
